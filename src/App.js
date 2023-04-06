842b8ae298b747023cbb3cc563003a9bd00a065a
it-ems <emrem.oruc@hotmail.com>
2023-04-06 01:07:52 +0000

    Update 8 files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./navbar.js";
 
 function App() {
   return (
-    <div className="Rechnungssoftware">
-      <header className="rechnungssoftware">
-        <h1 className="HEins"> Rechnungssoftware </h1>
-            
-    
-      </header>
+    <div className="bg-gray-700 p-5">
+      <navbar />
+      <div className="px-5 text-center">
+        <header className="rechnungssoftware">
+          <h1 className="text-4xl text-gray-300"> Prototype V1.0 </h1>
+          <p className="italic text-gray-300"> Hier entsteht eine App </p>
+        </header>
+      </div>
+      <navbar />
     </div>
   );
 }
---
+++ src/navbar.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react"
+
+function navbar() {
+  return (
+    <div className="liste">
+      <ul>
+        <li>Anmelden</li>
+        <li>Home</li>
+        <li>Service</li>
+        <li>Support</li>
+      </ul>
+    </div>
+  );
+}
+
+export default navbar;
